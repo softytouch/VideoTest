c5899ad5825469f0445c7ad6535797b665c1c43f
softytouch <36747426+softytouch@users.noreply.github.com>
2018-07-10 14:40:23 -0700

    Update MainActivity.java
    bugs fixed

--- app/src/main/java/com/example/softtouch/myapplication/MainActivity.java
+++ app/src/main/java/com/example/softtouch/myapplication/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        for(int 1; 1 < 0; 1++)
+            (
+            //this is what i want to do.......
+        );
     }
 }
